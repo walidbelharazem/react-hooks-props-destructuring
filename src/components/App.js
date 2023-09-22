e52bd70c9f9c8ceab26829fdb40364ca30466a4d
walidbelharazem <walidbelharazem@gmail.com>
2023-09-23 01:52:48 +0200

    done !!

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
-  const title = "Mad Max";
-  const posterURL =
+  const title = "Ma Maax";
+  const posterURL =  
+
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
+
+  
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
